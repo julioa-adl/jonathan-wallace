090b01afd0cad9b3056ef40eefbec01ae442dcc1
Júlio Adler <julioader@gmail.com>
2023-02-14 10:43:12 -0300

    add detalhes

--- src/pages/services/services-components/CardServices.jsx
+++ src/pages/services/services-components/CardServices.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Button from 'react-bootstrap/Button';
+import { BsArrowRight } from "react-icons/bs";
+
+
 function CardServices({ img, title, description, cn, idN }) {
   return (
     <div className={ cn } id={ idN }>
@@ -10,6 +14,14 @@ function CardServices({ img, title, description, cn, idN }) {
         <h2>{ title }</h2>
         <hr/>
         <p>{ description }</p>
+        <div>
+          <Button variant="outline-primary" size="sm"
+            href='https://api.whatsapp.com/send?phone=351927082012&text=Ol%C3%A1,%20gostaria%20de%20solicitar%20um%20or%C3%A7amento'
+            target='blank'
+          >
+            Contrate Agora <BsArrowRight/>
+          </Button>{' '}
+        </div>
       </div>
     </div>
   )
