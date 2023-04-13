7a0088ef8233bb2d12935946f3d3ada5eac0d685
Júlio Adler <julioader@gmail.com>
2023-04-13 15:23:58 -0300

    att card alpinism para os icones do header

--- src/pages/home/home-components/CardHeader.jsx
+++ src/pages/home/home-components/CardHeader.jsx
@@ -4,6 +4,7 @@ import lampada from '../../../images/icons-card-header/lampada.webp';
 import torneira from '../../../images/icons-card-header/torneira.webp';
 import manutencao from '../../../images/icons-card-header/manutencao.webp';
 import pintura from '../../../images/icons-card-header/pintura.webp';
+import alpinism from '../../../images/icons-card-header/alpinism.webp';
 
 function CardHeader() {
   return (
@@ -21,6 +22,7 @@ function CardHeader() {
       <div className='card-icon torneira'><img alt='torneira' src={ torneira }/></div>
       <div className='card-icon manutencao'><img alt='manutencao' src={ manutencao }/></div>
       <div className='card-icon pintura'><img alt='pintura' src={ pintura }/></div>
+      <div className='card-icon alpinism'><img alt='alpinism' src={ alpinism }/></div>
     </div>
   )
 }
